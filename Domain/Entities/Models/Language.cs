59828ccf7acf64452e37d9b4d40590b67eb9e388
1292272491 <1292272491@NADI-722>
2022-12-08 10:08:08 +0330

    Creating Models

---
+++ Domain/Entities/Models/Language.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class Language
+    {
+        [Column("LanguageId")]
+        public Guid Id { get; set; }
+   
+        [MaxLength(40)]
+        public string? Name { get; set; }
+
+        [MaxLength(3)]
+        public string? Abbreviation { get; set; }
+
+        [MaxLength(120)]
+        public string? PathFlag { get; set; }
+
+        [ForeignKey(nameof(PersonalInfo))]
+        public Guid PersonalInfoId { get; set; }
+        public PersonalInfo? PersonalInfo { get; set; }
+    }
+}
---
+++ Domain/Entities/Models/PersonalInfo.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class PersonalInfo
+    {
+        [Column("PersonalInfoId")]
+        public Guid Id { get; set; }
+
+        [MaxLength(30)]
+        [Required]
+        public string? Name { get; set; }
+
+        [MaxLength(30)]
+        [Required]
+        public string? Family { get; set; }
+
+        [MaxLength(40)]
+        [Required]
+        public string? Title { get; set; }
+
+        [MaxLength(30)]
+        public string? Nationality { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public int MaritalStatus { get; set; }
+
+        [MaxLength(30)]
+        public string? Residence { get; set; }
+
+        [MaxLength(20)]
+        public string? Phone { get; set; }
+
+        [MaxLength(20)]
+        public string? Mobile { get; set; }
+
+        [MaxLength(40)]
+        public string? Email { get; set; }
+
+        [MaxLength(100)]
+        public string? Address { get; set; }
+
+        [ForeignKey(nameof(PersonalInfo))]
+        public Guid LanguageId { get; set; }
+        public Language? Language { get; set; }
+
+    }
+}
