6bd9ebc9e13e50a42fab8c17a66bec0c046b366f
behrouznd <behrouz1986@gmail.com>
2022-12-09 18:20:56 +0330

    Context Class and Database connection

---
+++ Infrastructure/Repository/RepositoryContext.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Language>?   Languages { get; set; }
+        public DbSet<PersonalInfo>?  PersonalInfos { get; set; }
+        public DbSet<Certificate>?   Certificates { get; set; }
+        public DbSet<Contact>?   Contacts { get; set; }
+        public DbSet<Education>?   Educations { get; set; }
+        public DbSet<Experience>?   Experiences { get; set; }
+        public DbSet<Portfolio>?   Portfolios { get; set; }
+        public DbSet<Skill>? Skills { get; set; }
+        public DbSet<SkillGroup>? SkillGroups { get; set; }
+        public DbSet<SocialMedia>? SocialMedias { get; set; }
+    }
+}
---
+++ PersonalProfile/ContextFacory/RepositoryContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Repository;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace PersonalProfile.ContextFacory
+{
+    public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+    {
+        public RepositoryContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<RepositoryContext>()
+                .UseSqlServer(configuration.GetConnectionString("SqlConnection"));
+
+            return new RepositoryContext(builder.Options);
+             
+        }
+    }
+}
